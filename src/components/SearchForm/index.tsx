cc14e6a6c7dbbcab1f7a3062b321e56e08a6158e
jones-bass <jonesbass.tb@gmail.com>
2023-03-15 07:44:02 -0300

    Elemento de Busca

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,13 +1,37 @@
 import { BsSearch } from 'react-icons/bs'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
 
 import { SearchFormContainer } from './styles'
+import { useAuth } from '../../hooks/useAuth'
+
+const searchFromSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFromSchema>
 
 export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Buscar conteúdo" />
+  const { fetchGithub } = useAuth()
 
-      <button type="submit">
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFromSchema),
+  })
+
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await fetchGithub(data.query)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input type="text" placeholder="Buscar conteúdo" {...register('query')} />
+
+      <button type="submit" disabled={isSubmitting}>
         <BsSearch size={20} />
         Buscar
       </button>
